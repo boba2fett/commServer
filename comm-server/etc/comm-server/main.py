b2173ca4f529fd8c973f66a962f2fb49dad37428
boba2fett <boba2002fett@gmail.com>
2020-10-30 21:42:44 +0100

    asdf

--- comm-server/etc/comm-server/main.py
+++ comm-server/etc/comm-server/main.py
@@ -5,22 +5,25 @@ from server import Server
 import ssl
 import json
 import socket
+import logging
+from systemd.journal import JournalHandler
+
+log = logging.getLogger("comm-server")
+log.addHandler(JournalHandler())
+log.setLevel(logging.INFO)
 
 with open('settings.json', 'r') as json_file:
     config = json.load(json_file)
+
+log.info("settings read")
 
 class HTTPServerV6(HTTPServer):
     address_family = socket.AF_INET6
 
 if __name__ == '__main__':
-    httpd = HTTPServer((config["HOST_NAME"], config["PORT_NUMBER"]), Server)
+    httpd = HTTPServerV6((config["HOST_NAME"], config["PORT_NUMBER"]), Server)
     httpd.socket = ssl.wrap_socket (httpd.socket, 
         keyfile=f"/etc/letsencrypt/live/{config['domain']}/privkey.pem", 
         certfile=f"/etc/letsencrypt/live/{config['domain']}/fullchain.pem", server_side=True)
-    print(time.asctime(), 'Server Starts - %s:%s' % (config["HOST_NAME"], config["PORT_NUMBER"]))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print(time.asctime(), 'Server Stops - %s:%s' % (config["HOST_NAME"], config["PORT_NUMBER"]))
+    log.info('Server Starts - %s:%s' % (config["HOST_NAME"], config["PORT_NUMBER"]))
+    httpd.serve_forever()
--- comm-server/etc/comm-server/server.py
+++ comm-server/etc/comm-server/server.py
@@ -2,10 +2,17 @@ import os
 
 from http.server import BaseHTTPRequestHandler
 
+from urllib.parse import urlparse, parse_qs
 from response.commHandler import CommHandler
 from response.badRequestHandler import BadRequestHandler
 from response.forbiddenRequestHandler import ForbiddenRequestHandler
 import json
+import logging
+from systemd.journal import JournalHandler
+
+log = logging.getLogger("comm-server")
+log.addHandler(JournalHandler())
+log.setLevel(logging.INFO)
 
 with open('settings.json', 'r') as json_file:
     config = json.load(json_file)
@@ -15,12 +22,20 @@ class Server(BaseHTTPRequestHandler):
         return
 
     def do_GET(self):
-        split_path = os.path.splitext(self.path)
-        if split_path[0][1::] in os.listdir("routes"):
-            handler = CommHandler()
-            handler.find(self.path)
+        parsed_path = urlparse(self.path)
+        params=parse_qs(parsed_path.query)
+        real_path=parsed_path.path
+
+        if 't' not in params or params['t'][0]!=config["token"]:
+            handler=ForbiddenRequestHandler()
+            log.warn(f"Unautherised: {self.client_address}")
         else:
-            handler = BadRequestHandler()
+            if real_path[1::] in os.listdir("routes"): # prefix is still /
+                handler = CommHandler()
+                handler.find(real_path)
+            else:
+                handler = BadRequestHandler()
+ 
         self.respond({
             'handler': handler
         })
@@ -28,15 +43,17 @@ class Server(BaseHTTPRequestHandler):
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
-        split_path = os.path.splitext(self.path)
-        print(split_path)
-        print(post_body)
+        
+        parsed_path = urlparse(self.path)
+        real_path=parsed_path.path
+        
         if post_body!=bytes(config["token"],"utf8"):
             handler=ForbiddenRequestHandler()
+            log.warn(f"Unautherised: {self.client_address}")
         else:
-            if split_path[0][1::] in os.listdir("routes"):
+            if real_path[1::] in os.listdir("routes"):
                 handler = CommHandler()
-                handler.find(self.path)
+                handler.find(real_path)
             else:
                 handler = BadRequestHandler()
  
